b678a6c9d9a693a35b51b0f38ab1b19c39a80ec7
StepanKobrii18321832 <esestepan2000@gmail.com>
2020-06-08 21:13:41 +0300

    create

---
+++ Main.cs
@@ -0,0 +1,13 @@
+using System; // підключення "бібліотеки"
+
+namespace FirstProgramm // оголошення нової "бібліотеки" 
+{
+    class FirstProgramm
+    {
+        static void Main(string[] args) // метод
+        {
+            Console.WriteLine("programm start");
+            Console.WriteLine(DateTime.Now); // Теперішній час
+        }
+    }
+}
